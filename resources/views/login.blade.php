bf15b0fcccab05c5fa96a63b2e881873f875c435
syha-studio <syauqillah0908@gmail.com>
2024-08-02 11:32:16 +0700

    login Done, next Nasabah UI

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -46,17 +46,17 @@
                         @csrf
                         <x-form-input type="email" name="email" id="email" label="Email" placeholder="Masukkan Email" autofocus/>
                         <x-form-input type="password" name="password" id="password" label="Password" placeholder="••••••••"/>
-                        <div class="flex items-center justify-end">
-                            {{-- <div class="flex items-start">
+                        {{-- <div class="flex items-center justify-between">
+                            <div class="flex items-start">
                                 <div class="flex items-center h-5">
                                   <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
                                 </div>
                                 <div class="ml-3 text-sm">
                                   <label for="remember" class="text-gray-500 dark:text-gray-300">Ingat Saya</label>
                                 </div>
-                            </div> --}}
+                            </div>
                             <a href="#" class="text-sm font-medium text-design-secondary hover:underline dark:text-primary-500">Lupa Password?</a>
-                        </div>
+                        </div> --}}
                         <button type="submit" class="w-full text-design-white bg-design-primary hover:bg-green-200 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Masuk</button>
                         <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                             Belum punya akun? Yuk, <a href="/register" class="font-medium text-design-primary hover:underline dark:text-primary-500">Daftar</a> !
