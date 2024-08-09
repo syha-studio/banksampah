937116f11764b1032bb3b55364687bf6e77ca11a
syha-studio <syauqillah0908@gmail.com>
2024-08-09 21:59:01 +0700

    modified:   resources/views/components/layout-admin.blade.php 	modified:   resources/views/components/layout-nasabah.blade.php 	modified:   resources/views/components/layout.blade.php

--- resources/views/components/layout-nasabah.blade.php
+++ resources/views/components/layout-nasabah.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite(['resources/css/app.css','resources/js/app.js'])
     <title>{{ $title }}</title>
+    <link rel="icon" href="/img/banksbimalogo.png">
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
